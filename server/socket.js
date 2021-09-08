71f5aa853224bdef3c8d87d795b9d1f44e9f2aed
Jett Fox de Ruyter <foxderuyter.jett@gmail.com>
2021-09-09 01:16:28 +1000

    fixed the glitch with the outdated socket code

--- server/socket.js
+++ server/socket.js
@@ -9,7 +9,6 @@ module.exports = {
         chat.on('connection', (socket) => {
             console.log("client connected")
             socket.on('message', (message)=>{
-                console.log(message, socketChannel, socket.id)
                 for (let i=0; i<socketChannel.length; i++){
                     console.log('server', socketChannel[i][0])
                     if (socketChannel[i][0] == socket.id){
@@ -25,56 +24,48 @@ module.exports = {
             });
 
             socket.on('channellist', (m)=>{
-                console.log('channellist', m)
                 chat.emit('channellist', JSON.stringify(channels));
-                console.log(channels)
             })
 
             socket.on('numusers', (channel)=>{
-                console.log('numusers', channel)
                 var usercount = 0;
                 for (let i=0; i<socketChannelnum.length; i++){
                     
                     if(socketChannelnum[i][0] == channel){
                         
                         usercount = socketChannelnum[i][1];
-                        console.log('usercount', usercount)
                     }
                 }
                 chat.in(channel).emit('numusers', usercount);
             });
 
             socket.on('joinChannel', (channel)=>{
-                console.log("join channel", channel)
+
                 if (channels.includes(channel)){
-                    socket.join(channel, ()=>{
-                        console.log("join channel", channel)
-                        var inchannelSocketarray = false
-                        for (let i=0; i<socketChannel.length; i++){
-                            if (socketChannel[i][0]==socket.id){
-                                socketChannelnum[i][1] = channel;
-                                inchannel = true;
+                    socket.join(channel)
+                    var inchannelSocketarray = false
+                    for (let i=0; i<socketChannel.length; i++){
+                        if (socketChannel[i][0]==socket.id){
+                            socketChannelnum[i][1] = channel;
+                            inchannel = true;
+                        }
+                    }
+
+                    if (inchannelSocketarray == false){
+                        socketChannel.push([socket.id, channel]);
+                        var haschannelnum = false;
+                        for (let j=0; j<socketChannelnum.length; j++){
+                            if (socketChannelnum[j][0]==channel){
+                                socketChannelnum[j][1] = socketChannelnum[j][1] +1;
+                                haschannelnum = true;
                             }
                         }
 
-                        if (inchannelSocketarray == false){
-                            socketChannel.push([socket.id, channel]);
-                            var haschannelnum = false;
-                            for (let j=0; j<socketChannelnum.length; j++){
-                                if (socketChannelnum[j][0]==channel){
-                                    socketChannelnum[j][1] = socketChannelnum[j][1] +1;
-                                    haschannelnum = true;
-                                }
-                            }
-    
-                            if (haschannelnum == false){
-                                socketChannelnum.push([channel, 1]);
-                            }
+                        if (haschannelnum == false){
+                            socketChannelnum.push([channel, 1]);
                         }
-                        chat.in(channel).emit("notice", "A new user has joined");
-                        
-                        
-                    });
+                    }
+                    chat.in(channel).emit("notice", "A new user has joined");
                     return chat.in(channel).emit("joined", channel);
                 }
             });
